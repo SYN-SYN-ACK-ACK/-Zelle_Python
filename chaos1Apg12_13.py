05495402e64fb1b98ac96185fe57da7f8ec30a7c
SYN-SYN-ACK-ACK <guitarsandcellos@gmail.com>
2012-12-30 14:04:53 -0800

    Create chaos1Apg12_13.py
    # A simple program illustrating chaotic behavior.
    # Zelle page 12-13

---
+++ chaos1Apg12_13.py
@@ -0,0 +1,13 @@
+# File: chaos1Apg12_13.py
+# A simple program illustrating chaotic behavior.
+# Zelle page 12-13
+
+def main():
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x * (1 - x)
+        print(x)
+
+
+main()
